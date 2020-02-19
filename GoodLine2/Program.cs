4061a0bd0242314f6e9739d959a0f4f96a297607
MrDeVelopMe <61236894+MrDeVelopMe@users.noreply.github.com>
2020-02-20 01:20:19 +0700

    Add files via upload

---
+++ GoodLine2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GoodLine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] textWrite = text.Split(' ');
+            Array.Sort(textWrite);
+            for (int i = 0; i < textWrite.Length; i++)
+            {
+                Console.WriteLine(textWrite[i]);
+            }
+        }
+    }
+}
