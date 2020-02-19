0db81776e284c39ce2de8db31adce82f6b93e39c
MrDeVelopMe <61236894+MrDeVelopMe@users.noreply.github.com>
2020-02-20 01:21:08 +0700

    Add files via upload

---
+++ GoodLine3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GoodLine3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = 0;
+            string text = Console.ReadLine();
+            string[] textWrite = text.Split(' ');
+            Array.Sort(textWrite);
+            for (int i = 0; i < textWrite.Length; i++)
+            {
+                for (int j = textWrite.Length - 1; j > i; j--)
+                {
+                    if (textWrite[i] == textWrite[j])
+                    {
+                        Array.Clear(textWrite, j, 1);
+                        quantity++;
+                    }
+                }
+            }
+            for (int i = 0; i < textWrite.Length - 1; i++)
+            {
+                if (textWrite[i] == "")
+                {
+                    string trade = textWrite[i];
+                    textWrite[i] = textWrite[i + 1];
+                    textWrite[i + 1] = trade;
+                }
+            }
+            Array.Resize(ref textWrite, textWrite.Length - quantity);
+            for (int i = 0; i < textWrite.Length; i++)
+            {
+                Console.WriteLine(textWrite[i]);
+            }
+        }
+    }
+}
