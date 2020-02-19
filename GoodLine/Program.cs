8a387113dd1262b60cdfa850d504cd50262c0b86
MrDeVelopMe <61236894+MrDeVelopMe@users.noreply.github.com>
2020-02-20 01:19:06 +0700

    Add files via upload

---
+++ GoodLine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GoodLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = 0;
+            string text = Console.ReadLine();
+            string []textWrite = text.Split(' ');
+            for (int i = 0; i < textWrite.Length; i++)
+            {
+                    Console.WriteLine(textWrite[i]);
+            }
+        }
+    }
+}
